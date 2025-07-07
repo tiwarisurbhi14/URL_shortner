e39512608ef5d26b8a703ff8c26a45ee10421769
surbhi tiwari <tiwarisurbhi45@gmail.com>
2025-07-08 00:05:51 +0530

    fixes

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const app=express();
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://url-shortner-frontend-1vhd.onrender.com', 
     credentials: true 
 }));
 app.use(express.urlencoded({extended:true}));
