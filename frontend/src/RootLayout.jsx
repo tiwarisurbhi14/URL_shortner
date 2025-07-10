c9c484b3f0bbff14ff3dd1a4aaee19603fe357df
surbhi tiwari <tiwarisurbhi45@gmail.com>
2025-07-10 23:26:09 +0530

    rootLayout causing error

--- frontend/src/RootLayout.jsx
+++ frontend/src/RootLayout.jsx
@@ -1,48 +1,15 @@
-import { Outlet } from "@tanstack/react-router";
-import Navbar from "./components/Navbar";
-import { useDispatch } from "react-redux";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { setUser, logout } from "./store/slice/authSlice";
+import { Outlet } from '@tanstack/react-router'
+import Navbar from './components/Navbar'
+
 
 const RootLayout = () => {
-  const dispatch = useDispatch();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await axios.get(
-          "https://url-shortner-backend-fytv.onrender.com/api/auth/me",
-          {
-            withCredentials: true,
-          }
-        );
-        dispatch(setUser(response.data.user));
-      } catch (error) {
-        dispatch(logout());
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchUser();
-  }, [dispatch]);
-
-  if (loading) {
-    return (
-      <div className="flex justify-center items-center h-screen text-gray-500">
-        Loading...
-      </div>
-    );
-  }
-
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
-  );
-};
 
-export default RootLayout;
+    <>
+      <Navbar/>
+      <Outlet/>
+    </>
+  )
+}
+
+export default RootLayout
