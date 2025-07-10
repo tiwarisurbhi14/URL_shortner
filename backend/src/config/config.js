54a4d3989dc10eac4acb60069c302ded8ec99e7d
surbhi tiwari <tiwarisurbhi45@gmail.com>
2025-07-10 23:18:36 +0530

    final commit maybe

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,6 +1,6 @@
 export const cookieOptions = {
   maxAge: 24*60 * 60 * 1000,
   httpOnly: true,
-  secure: true,
+  secure: process.env.NODE_ENV === 'production',
   sameSite: "None",
 };
--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,28 +1,37 @@
 import wrapAsync from "../utils/tryCatchWraper.js";
-import { registerUser,loginUser } from "../services/auth.service.js";
+import { registerUser, loginUser } from "../services/auth.service.js";
 import { cookieOptions } from "../config/config.js";
+import { verifyToken } from "../utils/helper.js";
 
 export const register = wrapAsync(async (req, res) => {
   const { name, email, password } = req.body;
-  const {token,user} = await registerUser(name, email, password);
-  req.user=user;
+  const { token, user } = await registerUser(name, email, password);
+  req.user = user;
   res.cookie("accessToken", token, cookieOptions);
-  res.status(200).json({user:user, message: "registered successfully" },token);
+  res
+    .status(200)
+    .json({ user: user, message: "registered successfully" }, token);
 });
 
 export const login = wrapAsync(async (req, res) => {
-    const { email, password } = req.body;
-    const {token,user}=await loginUser(email,password);
-    req.user=user;
-    res.cookie("accessToken",token,cookieOptions);
-    res.status(200).json({user:user, message: "logged in successfully" },token);
+  const { email, password } = req.body;
+  const { token, user } = await loginUser(email, password);
+  req.user = user;
+  res.cookie("accessToken", token, cookieOptions);
+  res
+    .status(200)
+    .json({ user: user, message: "logged in successfully" }, token);
 });
 
-export const logout = wrapAsync( async (req, res) => {
-    res.clearCookie("accessToken", cookieOptions)
-    res.status(200).json({message:"logout success"})
-})
+export const logout = wrapAsync(async (req, res) => {
+  res.clearCookie("accessToken", cookieOptions);
+  res.status(200).json({ message: "logout success" });
+});
 
-export const get_current_user = wrapAsync( async (req, res) => {
-    res.status(200).json({user:req.user})
-})
+export const get_current_user = wrapAsync(async (req, res) => {
+  const token = req.cookies.token;
+  if (!token) return res.status(401).json({ error: "Not authenticated" });
+  const user = verifyToken(token);
+  if (!user) return res.status(401).json({ error: "Invalid token" });
+  res.status(200).json({ user: req.user });
+});
