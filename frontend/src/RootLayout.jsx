b4c5994e35ea176ac26bb10df857497b5f32c99d
surbhi tiwari <tiwarisurbhi45@gmail.com>
2025-07-10 18:26:53 +0530

    user fetching issue

--- frontend/src/RootLayout.jsx
+++ frontend/src/RootLayout.jsx
@@ -1,14 +1,31 @@
-import { Outlet } from '@tanstack/react-router'
-import Navbar from './components/Navbar'
-
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import axios from "axios";
+import { setUser, logout } from "./store/slice/authSlice";
 
 const RootLayout = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await axios.get("https://url-shortner-backend-fytv.onrender.com/api/auth/me", {
+          withCredentials: true, 
+        });
+        dispatch(setUser(res.data.user));
+      } catch (err) {
+        dispatch(logout());
+        console.error("Failed to fetch session:", err.message);
+      }
+    };
+
+    fetchUser();
+  }, [dispatch]);
+
   return (
     <>
-      <Navbar/>
-      <Outlet/>
+      <Navbar />
+      <Outlet />
     </>
-  )
-}
-
-export default RootLayout
+  );
+};
