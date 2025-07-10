bb3b8376fcaf7d6c3dd901955b4924f2edf68908
surbhi tiwari <tiwarisurbhi45@gmail.com>
2025-07-10 17:24:30 +0530

    added a new function in AUTHSLICE

--- frontend/src/store/slice/authSlice.js
+++ frontend/src/store/slice/authSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   user: null,
@@ -6,7 +6,7 @@ const initialState = {
 };
 
 const authSlice = createSlice({
-  name: 'auth',
+  name: "auth",
   initialState,
   reducers: {
     login: (state, action) => {
@@ -17,8 +17,12 @@ const authSlice = createSlice({
       state.user = null;
       state.isAuthenticated = false;
     },
+    setUser: (state, action) => {
+      state.user = action.payload;
+      state.isAuthenticated = true;
+    },
   },
 });
 
-export const { login,logout } = authSlice.actions;
+export const { login, logout,setUser } = authSlice.actions;
 export default authSlice.reducer;
