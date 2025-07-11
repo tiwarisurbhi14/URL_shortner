42a15817d400485fa5d85c5e70b2b9ddf17dcdef
surbhi tiwari <tiwarisurbhi45@gmail.com>
2025-07-11 12:19:00 +0530

    latest commits

--- frontend/src/RootLayout.jsx
+++ frontend/src/RootLayout.jsx
@@ -1,27 +1,9 @@
 import { Outlet } from '@tanstack/react-router'
 import Navbar from './components/Navbar'
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { setUser, logout } from "./store/slice/authSlice";
-import { getCurrentUser } from "./api/user.api";
+
 
 
 const RootLayout = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await getCurrentUser(); 
-        dispatch(setUser(response.user)); 
-      } catch (error) {
-        dispatch(logout());
-        console.error("User not authenticated:", error);
-      }
-    };
-
-    fetchUser();
-  }, []);
   return (
 
     <>
